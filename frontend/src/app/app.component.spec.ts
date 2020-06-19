1d92b794c088d6939631f50b072a956290b7c571
Vikas <vkas31215@gmail.com>
2020-06-19 18:04:23 +0530

    changed the frontend

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'to-do-app'`, () => {
+  it(`should have as title 'user-profile'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('to-do-app');
+    expect(app.title).toEqual('user-profile');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to to-do-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to user-profile!');
   });
 });
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserFormCreateComponent } from './components/user-form-create/user-form-create.component';
+import { UserFormEditComponent } from './components/user-form-edit/user-form-edit.component';
+import { UserFormViewComponent } from './components/user-form-view/user-form-view.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { HomeComponent } from './components/home/home.component';
+import { ToDoComponent } from './components/to-do/to-do.component';
+import { TodoFormCreateComponent } from './components/todo-form-create/todo-form-create.component';
+import { TodoFormEditComponent } from './components/todo-form-edit/todo-form-edit.component';
+import { TodoFormViewComponent } from './components/todo-form-view/todo-form-view.component';
+import { AuthgaurdService } from './services/authgaurd.service';
+import { HeaderComponent } from './components/header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
+    UserFormComponent,
+    UserFormCreateComponent,
+    UserFormEditComponent,
+    UserFormViewComponent,
+    RegisterFormComponent,
+    LoginFormComponent,
+    HomeComponent,
+    ToDoComponent,
+    TodoFormCreateComponent,
+    TodoFormEditComponent,
+    TodoFormViewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
